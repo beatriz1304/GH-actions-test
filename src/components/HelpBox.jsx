645230a7e6a91dbc6ac6de50e9646e656568044f
Beatriz da Silveira <beatriz1304@gmail.com>
2022-12-03 18:38:15 +0100

    Fix lint errors

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,12 +1,19 @@
-import './HelpBox.css';
+import { string } from 'prop-types'
+
+import './HelpBox.css'
 
 function HelpBox({ title, text }) {
   return (
-    <article className="help-box">
+    <article className='help-box'>
       <h2>{title}</h2>
       <p>{text}</p>
     </article>
-  );
+  )
 }
 
-export default HelpBox;
+HelpBox.propTypes = {
+  title: string,
+  text: string,
+}
+
+export default HelpBox
